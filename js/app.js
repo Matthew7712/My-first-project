94e61d655a17114494d37be4580c37e7e067d0a4
Matthew7712 <erbzvdxkbnf@gmail.com>
2023-09-14 23:30:32 +0300

    This is First Build my project

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('click',documentClick);
+
+function documentClick(e){
+    const targetItem = e.target;
+
+    if(targetItem.closest('.icon-menu')){
+        document.documentElement.classList.toggle('menu-open');
+    }
+}
+document.querySelector('.icon-menu').addEventListener('click', function() {
+    document.querySelector('.menu__body').classList.toggle('menu__open');
+    document.querySelector('.icon-menu').classList.toggle('menu__open');
+  });
